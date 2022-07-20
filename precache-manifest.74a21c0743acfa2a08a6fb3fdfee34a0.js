d1901da6db2de04a02624bf39efc42b2cce6bbab
sidh0o <96336121+sidh0o@users.noreply.github.com>
2022-07-21 01:42:19 +0530

    Updates

---
+++ precache-manifest.74a21c0743acfa2a08a6fb3fdfee34a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7198b25e797a5caf85ee527aae1cbbe0",
+    "url": "/keeper-App/index.html"
+  },
+  {
+    "revision": "f9da775e41ab7372cddc",
+    "url": "/keeper-App/static/js/2.398dbefe.chunk.js"
+  },
+  {
+    "revision": "22474e6100ffa327613c",
+    "url": "/keeper-App/static/js/main.fde22f46.chunk.js"
+  },
+  {
+    "revision": "6797eb05abd1541553bf",
+    "url": "/keeper-App/static/js/runtime-main.80d98342.js"
+  }
+]);
